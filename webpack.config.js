3a8396bf653f637b3f31907dfea91f43ee2872cb
yulyakuzich <uljaonoff@gmail.com>
2023-08-03 17:08:17 +0200

    fix: husky setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
   entry: path.resolve(__dirname, './src/index.ts'),
@@ -36,6 +37,7 @@ const baseConfig = {
       filename: 'index.html',
     }),
     new CleanWebpackPlugin(),
+    new ESLintPlugin({ extensions: 'ts' }),
   ],
 };
 
