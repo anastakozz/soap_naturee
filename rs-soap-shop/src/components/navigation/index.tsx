3c3ba4188a8a78729dfd9a87001725374699f6d8
yulyakuzich <uljaonoff@gmail.com>
2023-08-11 18:54:01 +0300

    fix: fix link color, change favicon and title in html

--- rs-soap-shop/src/components/navigation/index.tsx
+++ rs-soap-shop/src/components/navigation/index.tsx
@@ -1,28 +1,28 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <nav className='flex mb-4 md:mb-0'>
+    <nav className="flex mb-4 md:mb-0">
       <NavLink
-        className='text-basicColor md:mr-md mr-4 hover:text-accentColor cursor-pointer dark:text-primaryColor dark:hover:text-accentDarkColor transition'
+        className="text-basicColor md:mr-md mr-4 hover:text-accentColor cursor-pointer dark:text-primaryColor dark:hover:text-accentColor transition"
         to={'/'}
       >
         Home
       </NavLink>
       <NavLink
-        className='text-basicColor md:mr-md mr-4 hover:text-accentColor cursor-pointer dark:text-primaryColor dark:hover:text-accentDarkColor transition'
+        className="text-basicColor md:mr-md mr-4 hover:text-accentColor cursor-pointer dark:text-primaryColor dark:hover:text-accentColor transition"
         to={'/our-products'}
       >
         Our products
       </NavLink>
       <NavLink
-        className='text-basicColor hover:text-accentColor cursor-pointer dark:text-primaryColor dark:hover:text-accentDarkColor transition'
+        className="text-basicColor hover:text-accentColor cursor-pointer dark:text-primaryColor dark:hover:text-accentDarkColor transition"
         to={'/about-us'}
       >
         About us
       </NavLink>
     </nav>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
