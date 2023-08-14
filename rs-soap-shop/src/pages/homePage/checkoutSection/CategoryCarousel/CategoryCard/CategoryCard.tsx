5b2eb8784b5f608bb730af5a5bfccab71bd60e0a
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-08-14 22:55:05 +0300

    fix: edit hover styles of CategoryCard

--- rs-soap-shop/src/pages/homePage/checkoutSection/CategoryCarousel/CategoryCard/CategoryCard.tsx
+++ rs-soap-shop/src/pages/homePage/checkoutSection/CategoryCarousel/CategoryCard/CategoryCard.tsx
@@ -3,13 +3,13 @@ import { CategoryCardProps } from '../../../../../lib/interfaces'
 
 export default function CategoryCard(item: CategoryCardProps) {
   return (
-    <Link to={item.link} className='flex flex-col content-center'>
+    <Link to={item.link} className='flex flex-col content-center text-basicColor dark:text-secondaryColor hover:text-accentColor dark:hover:text-accentColor'>
       <img
         className='w-[350px] transition rounded-normal mb-esm brightness-110 dark:brightness-75 saturate-[0.9] hover:filter-none dark:hover:filter-none'
         src={item.path}
         alt=''
       />
-      <p className='text-h4 text-center text-basicColor dark:text-secondaryColor font-semibold'>{item.name}</p>
+      <p className='text-h4 text-center  font-semibold'>{item.name}</p>
     </Link>
   )
 }
