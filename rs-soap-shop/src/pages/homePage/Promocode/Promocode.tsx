b8886b6d870edfd1f7c7036ae00ad19f9ae2a88e
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-08-16 12:02:05 +0300

    fix: edit promocode styles

--- rs-soap-shop/src/pages/homePage/Promocode/Promocode.tsx
+++ rs-soap-shop/src/pages/homePage/Promocode/Promocode.tsx
@@ -4,12 +4,14 @@ import ArrowSmallIcon from '../../../icons/arrowSmallIcon'
 
 export default function Promocode() {
   return (
-    <Link to={'/our-products'}>
-      <div className='flex items-center justify-center transition p-sm bg-accentColor dark:bg-accentDarkColor text-center text-greyLColor dark:text-basicColor hover:text-secondaryColor dark:hover:text-secondaryColor'>
+    <div className=' bg-accentColor dark:bg-accentDarkColor '>
+      <Link to={'/our-products'} className='max-w-[1440px] mx-auto flex items-center justify-center transition p-sm text-center text-greyLColor dark:text-basicColor hover:text-secondaryColor dark:hover:text-secondaryColor '>
         <SaleIcon></SaleIcon>
-        <span className='text-sm sm:text-base md:text-2xl lg:text-h3 font-bold'>Get a 5% discount on the promo code &quot;NATURE&quot;</span>
+        <span className='text-sm sm:text-base md:text-2xl lg:text-h3 font-bold'>
+          Get a 5% discount on the promo code &quot;NATURE&quot;
+        </span>
         <ArrowSmallIcon />
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
