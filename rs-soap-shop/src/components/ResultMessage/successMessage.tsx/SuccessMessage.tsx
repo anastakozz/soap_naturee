c70cceea9e24d90463b5a90bffd32365df22bf31
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-08-20 12:57:45 +0300

    fix: finish resolving rebase issues

--- rs-soap-shop/src/components/ResultMessage/successMessage.tsx/SuccessMessage.tsx
+++ rs-soap-shop/src/components/ResultMessage/successMessage.tsx/SuccessMessage.tsx
@@ -1,9 +1,13 @@
 export default function SuccessMessage() {
   return (
     <>
-      <button onClick={()=>{console.log('login and redirect')}}>
+      <button
+        onClick={() => {
+          console.log('login and redirect')
+        }}
+      >
         <div className='w-full h-full bg-grayLColor opacity-30 fixed z-10 top-0 left-0'></div>
-        <div className='z-20 bg-secondaryColor fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-normal px-sm py-big border-accentColor border-8 border-double'>
+        <div className='z-20 bg-secondaryColor dark:bg-grayLColor dark:text-secondaryColor fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-normal px-sm py-big border-accentColor dark:border-secondaryColor border-8 border-double'>
           <p className='text-h4'>Yahoooo!</p>
           <p>Your account has been succefully created!</p>
         </div>
--- rs-soap-shop/src/services/handleRegistration.ts
+++ rs-soap-shop/src/services/handleRegistration.ts
@@ -1,64 +1,27 @@
 import { RegistrationData, ResultProps } from '../lib/interfaces'
 import { createCustomer } from './registration.service'
 
-const testData: Partial<RegistrationData> = {
-  email: '26@2.com',
-  firstName: 'A',
-  secondName: 'A',
-  password: 'lalala',
-  date: '1950-09-09',
-  billingAddress: {
-    country: 'DE',
-    city: 'Milan',
-    street: 'AAA',
-    house: '111',
-    postalCode: '111',
-    isDefault: false
-  },
-  shippingAddress: {
-    country: 'DE',
-    city: 'Milan',
-    street: 'AAA',
-    house: '111',
-    postalCode: '111',
-    isDefault: true
+const countryId = {
+  Italy: 'IT',
+  Spain: 'ES',
+  Germany: 'DE'
+}
+
+function dataAdapter(data: RegistrationData): RegistrationData {
+  try {
+    const billingKey = data.billingAddress.country as keyof typeof countryId
+    const shippingKey = data.shippingAddress.country as keyof typeof countryId
+
+    data.billingAddress.country = countryId[billingKey]
+    data.shippingAddress.country = countryId[shippingKey]
+    return data
+  } catch (error) {
+    console.log(error)
   }
 }
 
-const countryId = {
-  Italy: 'IT',
-  Spain: 'ES',
-  Germany: 'DE'
-}
-
-function dataAdapter(data: RegistrationData): RegistrationData {
-  const billingKey = data.billingAddress.country as keyof typeof countryId
-  const shippingKey = data.shippingAddress.country as keyof typeof countryId
-
-  data.billingAddress.country = countryId[billingKey]
-  data.shippingAddress.country = countryId[shippingKey]
-  return data
-}
-
-const countryId = {
-  Italy: 'IT',
-  Spain: 'ES',
-  Germany: 'DE'
-}
-
-function dataAdapter(data: RegistrationData): RegistrationData {
-  const billingKey = data.billingAddress.country as keyof typeof countryId
-  const shippingKey = data.shippingAddress.country as keyof typeof countryId
-
-  data.billingAddress.country = countryId[billingKey]
-  data.shippingAddress.country = countryId[shippingKey]
-  return data
-}
-
-export async function handleRegistration(): Promise<ResultProps> {
-  const data = dataAdapter(testData)
-  const result = await createCustomer(data)
-  const data = dataAdapter(testData)
-  const result = await createCustomer(data)
+export async function handleRegistration(data: RegistrationData): Promise<ResultProps> {
+  const dataAdapted = dataAdapter(data)
+  const result = await createCustomer(dataAdapted)
   return result
 }
