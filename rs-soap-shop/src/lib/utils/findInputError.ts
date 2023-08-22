b22774c0a7ee857fdefc30dc2192102d4f238605
SlaVR7 <mylife0793@gmail.com>
2023-08-22 21:37:27 +0300

    chore: remove unused files

--- rs-soap-shop/src/lib/utils/findInputError.ts
+++
@@ -1,8 +0,0 @@
-export function findInputError(errors: Record<string, unknown>, name: string) {
-  const filtered = Object.keys(errors)
-    .filter(key => key.includes(name))
-    .reduce((cur, key) => {
-      return Object.assign(cur, { error: errors[key] })
-    }, {})
-  return filtered
-}
