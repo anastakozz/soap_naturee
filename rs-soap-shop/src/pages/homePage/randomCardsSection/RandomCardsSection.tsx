2b9fe9ddde75fddb2dbab30b4070e885c4dd4e29
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-08-25 16:01:42 +0300

    feat: undefined data checking on random saction

--- rs-soap-shop/src/pages/homePage/randomCardsSection/RandomCardsSection.tsx
+++ rs-soap-shop/src/pages/homePage/randomCardsSection/RandomCardsSection.tsx
@@ -11,7 +11,6 @@ async function getCardsData(): Promise<ProductCardProps[]> {
   if (data) {
     const shuffledData = shuffleProducts(data).slice(0, 6)
     const dataAdapted = shuffledData.map((product: Product) => toCardAdapter(product))
-    console.log(dataAdapted)
     return dataAdapted
   }
 }
@@ -26,20 +25,27 @@ const items = await getCardsData()
 
 export default function RandomCardsSection() {
   return (
-    <div className='bg-primaryColor dark:bg-grayMColor h-auto p-sm text-center px-big flex flex-col items-center'>
-      <h3 className='text-basicColor dark:text-secondaryColor text-h3 text-center font-bold'>You may like it</h3>
-      <div className='flex flex-wrap justify-around mt-sm max-w-[1245px] pb-sm '>
-        {items.map((item, index): ReactNode => {
-          return (
-            <div key={index} className='mb-sm mx-4'>
-              <Card {...item} />
-            </div>
+    <>
+      {items ? (
+        <div className='bg-primaryColor dark:bg-grayMColor h-auto p-sm text-center px-big flex flex-col items-center'>
+          (<h3 className='text-basicColor dark:text-secondaryColor text-h3 text-center font-bold'>You may like it</h3>
+          <div className='flex flex-wrap justify-around mt-sm max-w-[1245px] pb-sm '>
+            {items.map((item, index): ReactNode => {
+              return (
+                <div key={index} className='mb-sm mx-4'>
+                  <Card {...item} />
+                </div>
+              )
+            })}
+          </div>
+          <Link to={'./our-products'}>
+            <EmptyButton>Show More</EmptyButton>
+          </Link>
           )
-        })}
-      </div>
-      <Link to={'./our-products'}>
-        <EmptyButton>Show More</EmptyButton>
-      </Link>
-    </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   )
 }
