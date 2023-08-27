0cfb733e31ed83a4c96e2ae475cf1d30872f26f4
yulyakuzich <uljaonoff@gmail.com>
2023-08-27 21:59:34 +0200

    fix:adderror hanling

--- rs-soap-shop/src/services/account.service.ts
+++ rs-soap-shop/src/services/account.service.ts
@@ -1,11 +1,15 @@
-import axios from 'axios'
-import { apiUrl, projectKey } from '../lib/constants'
-
-const HEADERS = {
-  Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token')).access_token,
-  'Content-Type': 'application/x-www-form-urlencoded'
-}
+import axios from 'axios';
+import { apiUrl, projectKey } from '../lib/constants';
 
 export function getAccountData() {
-  return axios.get(`${apiUrl}/${projectKey}/me`, { headers: HEADERS })
+  try {
+    return axios.get(`${apiUrl}/${projectKey}/me`, {
+      headers: {
+        Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token')).access_token,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
