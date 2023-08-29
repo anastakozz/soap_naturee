359771608b1164047cd751d962e0f6e2df85c0e2
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-08-29 18:06:53 +0300

    feat: handle 1 picture case in modal slider

--- rs-soap-shop/src/components/SliderModal/SliderModal.tsx
+++ rs-soap-shop/src/components/SliderModal/SliderModal.tsx
@@ -1,14 +1,17 @@
 import CarouselDefault from '../carousel';
 
 export default function SliderModal(modal: { isVisible: boolean; paths: string[] }) {
-
   return (
     <>
       {modal.isVisible ? (
         <>
           <div className='bg-grayLColor/90 h-full w-full z-[60] absolute top-0 left-0'></div>
           <div className='absolute z-[70] top-center top-1/2 left-1/2 h-[80%] w-[80%] -translate-x-1/2 -translate-y-1/2'>
-            <CarouselDefault {...{ paths: modal.paths }}></CarouselDefault>
+            {modal.paths.length === 1 ? (
+              <img src={modal.paths[0]} className='rounded-xl object-cover h-full w-full'></img>
+            ) : (
+              <CarouselDefault {...{ paths: modal.paths }}></CarouselDefault>
+            )}
           </div>
           <div className='transition closing-icon absolute top-5 right-5 font-bold text-primaryColor hover:text-accentColor cursor-pointer z-[80]'>
             CLOSE
