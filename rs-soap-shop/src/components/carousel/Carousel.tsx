bca5c5788d58a276c70d8ccf0da436b075a73369
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-08-29 18:03:13 +0300

    feat: add modal slider

--- rs-soap-shop/src/components/carousel/Carousel.tsx
+++ rs-soap-shop/src/components/carousel/Carousel.tsx
@@ -5,14 +5,7 @@ export default function CarouselDefault(prop: { paths: string[] }) {
   return (
     <Carousel className='rounded-xl'>
       {prop.paths.map((item, index): ReactNode => {
-        return (
-          <img
-            key={`carousel-item-${index}`}
-            src={item}
-            alt=''
-            className='object-cover h-full w-full overflow-hidden'
-          />
-        );
+        return <img key={`carousel-item-${index}`} src={item} alt='' className='object-cover h-full w-full' />;
       })}
     </Carousel>
   );
--- rs-soap-shop/src/pages/detailedProductPage/detailedProductPage.tsx
+++ rs-soap-shop/src/pages/detailedProductPage/detailedProductPage.tsx
@@ -5,8 +5,10 @@ import { DetailsProps } from '../../lib/interfaces';
 import CarouselDefault from '../../components/carousel';
 import toDetailsAdapter from '../../lib/utils/productDataAdapters.ts/toDetailsAdapter';
 import EmptyButton from '../../components/buttons/emptyButton';
+import SliderModal from '../../components/SliderModal';
 
 function DetailedProductPage() {
+  const [isModalVisible, setModalVisibility] = useState(false);
   const [data, initProductData] = useState<DetailsProps | null>(null);
   const { key } = useParams();
 
@@ -21,15 +23,24 @@ function DetailedProductPage() {
     });
   }, []);
 
+  function toggleModal(event: React.MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!isModalVisible && target.tagName === 'IMG') {
+      setModalVisibility(!isModalVisible);
+    } else if (isModalVisible && target.classList.contains('closing-icon')) {
+      setModalVisibility(!isModalVisible);
+    }
+  }
+
   return (
     <div className='bg-primaryColor dark:bg-grayMColor'>
       {data ? (
         <div className='max-w-[1440px] mx-auto px-8 lg:px-big py-4'>
           <h1 className='text-h2 py-4 text-center md:text-left text-accentColor dark:text-primaryColor'>{data.name}</h1>
           <div className='flex items-center flex-col md:flex-row md:justify-between md:items-start'>
-            <div className='max-w-[400px] mx-auto h-[400px] md:mx-0 md:ml-10 mb-10'>
+            <div onClick={toggleModal} className='max-w-[400px] mx-auto h-[400px] md:mx-0 md:ml-10 mb-10'>
               {data.imgSources.length === 1 ? (
-                <img className='rounded-xl object-cover h-full w-full overflow-hidde' src={data.imgSources[0]}></img>
+                <img className='rounded-xl object-cover h-full w-full' src={data.imgSources[1]}></img>
               ) : (
                 <CarouselDefault {...{ paths: data.imgSources }}></CarouselDefault>
               )}
@@ -65,6 +76,9 @@ function DetailedProductPage() {
                 </div>
               </div>
             </div>
+          </div>
+          <div onClick={toggleModal}>
+            <SliderModal {...{ isVisible: isModalVisible, paths: data.imgSources }}></SliderModal>
           </div>
         </div>
       ) : (
