a7286db79b22cddec420743fc9af224648ec848c
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-08-29 18:34:26 +0300

    fix: mocked useNavigate for Card.test

--- rs-soap-shop/src/components/card/Card.test.tsx
+++ rs-soap-shop/src/components/card/Card.test.tsx
@@ -1,6 +1,14 @@
 import { ProductCardProps } from '../../lib/interfaces';
 import Card from './Card';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate
+}));
 
 const propsData: ProductCardProps = {
   productId: '12345',
@@ -14,6 +22,10 @@ const propsData: ProductCardProps = {
 };
 
 it('renders a card in DOM', () => {
-  const component = render(<Card {...propsData} />);
+  const component = render(
+    <BrowserRouter>
+      <Card {...propsData} />
+    </BrowserRouter>
+  );
   expect(component.getByTestId('card')).toBeInTheDocument();
 });
