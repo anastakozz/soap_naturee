16b38618a00c4d15f24e667e38c48ef347c23984
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-08-30 12:47:40 +0300

    fix: fix axios odule import error in jest

--- rs-soap-shop/src/pages/detailedProductPage/detailedProductPage.test.tsx
+++ rs-soap-shop/src/pages/detailedProductPage/detailedProductPage.test.tsx
@@ -2,11 +2,11 @@ import { render, screen } from '@testing-library/react';
 import DetailedProductPage from './detailedProductPage';
 import { BrowserRouter } from 'react-router-dom';
 
-it('adds renders detailedPage page in DOM', () => {
+it('renders detailedPage page in DOM', () => {
     render(
         <BrowserRouter>
           <DetailedProductPage />
         </BrowserRouter>
       );
-      expect(screen.queryByText('Tags')).toBeInTheDocument();
+      expect(screen.queryByText('Loading...')).toBeInTheDocument();
 })
