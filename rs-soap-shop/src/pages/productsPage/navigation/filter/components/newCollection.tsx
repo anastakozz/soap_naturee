e63b7032c329fe0a48b9a3926f4078872d7089d6
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-08-31 23:20:00 +0300

    feat: add basic price filtration

--- rs-soap-shop/src/pages/productsPage/navigation/filter/components/newCollection.tsx
+++ rs-soap-shop/src/pages/productsPage/navigation/filter/components/newCollection.tsx
@@ -1,10 +1,8 @@
 export function NewCollection() {
   return (
-    <form action='' className='flex content-center whitespace-nowrap'>
-      <input id={'newCollection'} type='checkbox'></input>
-      <label htmlFor={'newCollection'} className='px-2'>
-        New collection
-      </label>
+    <form action='' className="flex content-center whitespace-nowrap">
+      <input id={'newCollection'} type='checkbox' className="new-check"></input>
+      <label htmlFor={'newCollection'} className='px-2'>New collection</label>
     </form>
   );
 }
