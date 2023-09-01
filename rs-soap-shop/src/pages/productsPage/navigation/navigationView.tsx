b524161810d393b76ada993e6aae49554cdbbd14
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-09-01 23:49:55 +0300

    feat: add uniting of sort and filter queries

--- rs-soap-shop/src/pages/productsPage/navigation/navigationView.tsx
+++ rs-soap-shop/src/pages/productsPage/navigation/navigationView.tsx
@@ -3,10 +3,27 @@ import SortingView from './sorting/sortingView';
 import FilterView from './filter/filterView';
 import SearchView from './search/searchView';
 import Breadcrumb from '../../../components/BasicBreadcrumbs';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationViewProps } from '../../../lib/interfaces';
 
 export function NavigationView({ nav, changeQuery }: NavigationViewProps) {
+  const [filterQuery, setFilterQuery] = useState('');
+  const [sortQuery, setSortQuery] = useState('');
+
+  function updateFilterQuery(options: string) {
+    setFilterQuery(options);
+  }
+
+  function updateSortQuery(options: string) {
+    setSortQuery(options);
+  }
+
+  useEffect(() => {
+    if (filterQuery || sortQuery) {
+      changeQuery(`${filterQuery}&${sortQuery}`)
+    } 
+  }, [filterQuery, sortQuery]);
+
   return (
     <div className='bg-accentColor dark:bg-accentDarkColor text-primaryColor'>
       <div className='flex flex-wrap justify-between items-center max-w-[1440px] py-4 px-4 mx-auto lg:px-big'>
@@ -16,7 +33,7 @@ export function NavigationView({ nav, changeQuery }: NavigationViewProps) {
         </div>
 
         <SortingView />
-        <FilterView changeQuery={changeQuery} />
+        <FilterView changeQuery={updateFilterQuery} />
         <SearchView />
       </div>
     </div>
