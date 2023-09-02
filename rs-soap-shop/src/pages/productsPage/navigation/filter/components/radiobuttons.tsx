8a06ae884ccb9524fa91f5645d0fafe8c49db11b
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-09-02 12:33:41 +0300

    feat: style checkboxes

--- rs-soap-shop/src/pages/productsPage/navigation/filter/components/radiobuttons.tsx
+++ rs-soap-shop/src/pages/productsPage/navigation/filter/components/radiobuttons.tsx
@@ -2,13 +2,13 @@ export function Radiobuttons() {
   return (
     <form className={'whitespace-nowrap'} action=''>
       <div className=' pb-1 flex content-center'>
-        <input type='checkbox' className='product-check'></input>
+        <input type='checkbox' className='product-check accent-accentColor'></input>
         <label htmlFor='option1' className='px-2'>
           Product
         </label>
       </div>
       <div className=' pb-1 flex content-center'>
-        <input type='checkbox' className='set-check'></input>
+        <input type='checkbox' className='set-check accent-accentColor'></input>
         <label htmlFor='option2' className='px-2'>
           Set
         </label>
