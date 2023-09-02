6c767286b66586026d1545e459bb37a32519fcfd
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-09-02 13:54:56 +0300

    fix: change slider scrolling behavior and styles

--- rs-soap-shop/src/components/SliderModal/SliderModal.tsx
+++ rs-soap-shop/src/components/SliderModal/SliderModal.tsx
@@ -10,12 +10,14 @@ export default function SliderModal(modal: { isVisible: boolean; paths: string[]
     <>
       {modal.isVisible ? (
         <>
+          {scrollToTop()}
+          {(document.body.style.overflow = 'hidden')}
           <div className='bg-grayLColor/90 h-full w-full z-[60] absolute top-0 left-0'></div>
           <div className='absolute z-[70] top-center top-1/2 left-1/2 h-[80%] w-[80%] -translate-x-1/2 -translate-y-1/2'>
             {modal.paths.length === 1 ? (
-              <img src={modal.paths[0]} className='rounded-xl object-cover h-full w-full'></img>
+              <img src={modal.paths[0]} className='object-contain h-full w-full'></img>
             ) : (
-              <CarouselDefault {...{ paths: modal.paths }}></CarouselDefault>
+              <CarouselDefault {...{ paths: modal.paths, classes: 'w-full h-full object-contain' }}></CarouselDefault>
             )}
           </div>
           <div className='transition closing-icon absolute top-5 right-5 font-bold text-primaryColor hover:text-accentColor cursor-pointer z-[80]'>
@@ -23,7 +25,7 @@ export default function SliderModal(modal: { isVisible: boolean; paths: string[]
           </div>
         </>
       ) : (
-        <></>
+        <>{(document.body.style.overflow = 'visible')}</>
       )}
     </>
   );
--- rs-soap-shop/src/pages/detailedProductPage/detailedProductPage.tsx
+++ rs-soap-shop/src/pages/detailedProductPage/detailedProductPage.tsx
@@ -47,7 +47,7 @@ function DetailedProductPage() {
               {data.imgSources.length === 1 ? (
                 <img className='rounded-xl object-cover h-full w-full' src={data.imgSources[1]}></img>
               ) : (
-                <CarouselDefault {...{ paths: data.imgSources }}></CarouselDefault>
+                <CarouselDefault {...{ paths: data.imgSources, classes: 'h-full w-full object-cover' }}></CarouselDefault>
               )}
             </div>
             <div className='max-w-[400px] md:max-w-[60%] md:order-first'>
