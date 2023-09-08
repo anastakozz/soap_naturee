3d4178c1a60c008cf73ed084a7352123d259409d
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-09-08 22:54:24 +0300

    feat: set country for new carts

--- rs-soap-shop/src/services/handleCart.ts
+++ rs-soap-shop/src/services/handleCart.ts
@@ -8,14 +8,12 @@ export async function getCartId() {
     try {
       const cart = await getActiveCart(token);
       cartId = cart.data.id;
-      console.log(cart)
-      updateCart(token, cartId, cart.data.version);
-      console.log(cart);
     } catch {
       console.log('no active Cart');
       try {
         const cart = await createCart(token);
         cartId = cart.data.id;
+        updateCart(token, cartId, cart.data.version);
       } catch (err) {
         console.log(err);
       }
