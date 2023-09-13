1fb165379a5fb96b598854e2a5232bbaea96e168
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-09-13 13:12:26 +0300

    feat: mock localStorage for tests

--- rs-soap-shop/src/pages/detailedProductPage/detailedProductPage.test.tsx
+++ rs-soap-shop/src/pages/detailedProductPage/detailedProductPage.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import DetailedProductPage from './detailedProductPage';
 import { BrowserRouter } from 'react-router-dom';
+import { setAnonymousToLocalStorage } from '../../setupTests';
 
 it('renders detailedPage page in DOM', () => {
+  setAnonymousToLocalStorage();
   render(
     <BrowserRouter>
       <DetailedProductPage />
