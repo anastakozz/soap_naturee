d2ba1697eeb9edc14ed0e74242815439c3e19ba5
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-09-15 13:23:36 +0300

    feat: add animation to home page slider

--- rs-soap-shop/src/pages/homePage/checkoutSection/CategoryCarousel/CategoryCarousel.tsx
+++ rs-soap-shop/src/pages/homePage/checkoutSection/CategoryCarousel/CategoryCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, ReactNode, useEffect } from 'react';
 import ArrowIcon from '../../../../icons/arrowIcon';
 import { CategoryCardProps } from '../../../../lib/interfaces';
 import CategoryCard from './CategoryCard';
@@ -7,43 +7,62 @@ const items: CategoryCardProps[] = [
   { name: 'Candles', path: './images/candle.png', link: '/our-products/decor/candles' },
   { name: 'Soap', path: './images/soap.png', link: '/our-products/self-care/soap' },
   { name: 'Scrubs', path: './images/scrub.png', link: '/our-products/self-care/scrub' },
-  { name: 'Aroma sachet', path: './images/sachet.png', link: '/our-productsdecor/aroma-sachet' },
+  { name: 'Aroma sachet', path: './images/sachet.png', link: '/our-products/decor/aroma-sachet' },
   { name: 'Bath bombs', path: './images/bombs.png', link: '/our-products/self-care/bath-bomb' }
 ];
+const moveForward = 'animate-[slide_0.5s_linear]';
+const moveBackwards = 'animate-[slideBack_0.5s_linear]';
 
 export default function CategoryCarousel() {
-  const length = items.length;
+  const length = 5;
   const [index, setIndex] = useState(0);
-  const nextIndex = index === length - 1 ? 0 : index + 1;
-  const prevIndex = index === 0 ? length - 1 : index - 1;
+  const [animation, setAnimation] = useState<string>('');
+  const itemsToShow = Array(length)
+    .fill(index)
+    .map((item, index) => {
+      const newValue = item + index;
+      if (newValue >= items.length) {
+        return item + index - length;
+      }
+      return newValue;
+    });
 
   const handlePrevious = () => {
-    const newIndex = index - 1;
-    setIndex(newIndex < 0 ? length - 1 : newIndex);
+    setAnimation(moveBackwards);
+    setTimeout(() => {
+      const newIndex = index - 1;
+      setIndex(newIndex < 0 ? length - 1 : newIndex);
+      setAnimation('')
+    }, 500);
   };
 
   const handleNext = () => {
-    const newIndex = index + 1;
-    setIndex(newIndex >= length ? 0 : newIndex);
+    setAnimation(moveForward);
+    setTimeout(() => {
+      const newIndex = index + 1;
+      setIndex(newIndex >= length ? 0 : newIndex);
+      setAnimation('')
+    }, 500);
   };
 
   return (
-    <div data-testid='carousel' className='flex justify-around flex-wrap md:m-esm md:flex-nowrap'>
-      <button data-testid='prev-button' onClick={handlePrevious}>
+    <div data-testid='carousel' className='flex justify-center md:m-esm mt-4 flex-nowrap'>
+      <button data-testid='prev-button' onClick={handlePrevious} className='mr-4'>
         <ArrowIcon />
       </button>
-      <div className='flex justify-around w-full order-first md:order-none'>
-        <div className='hidden lg:block'>
-          <CategoryCard {...items[prevIndex]} />
-        </div>
-        <div>
-          <CategoryCard {...items[index]} />
-        </div>
-        <div className='hidden xl:block'>
-          <CategoryCard {...items[nextIndex]} />
+      <div className='overflow-hidden w-[370px] lg:w-[1110px]'>
+        <div className={`${animation} flex justify-around w-full`}>
+          {itemsToShow.map((elem, index): ReactNode => {
+            return (
+              <div key={`category-card ${index}`}>
+                <CategoryCard {...items[elem]} />
+              </div>
+            );
+          })}
         </div>
       </div>
-      <button data-testid='next-button' onClick={handleNext} className='transform rotate-180'>
+
+      <button data-testid='next-button' onClick={handleNext} className='transform rotate-180 ml-4'>
         <ArrowIcon />
       </button>
     </div>
