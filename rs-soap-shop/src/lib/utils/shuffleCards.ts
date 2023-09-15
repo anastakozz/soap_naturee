8e0a4531ff558ae589f295483323bfc9dc1c94bd
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-09-15 14:45:32 +0300

    feat: refactor ShuffleCards and add test

--- rs-soap-shop/src/lib/utils/shuffleCards.ts
+++ rs-soap-shop/src/lib/utils/shuffleCards.ts
@@ -1,6 +1,4 @@
-import { Product } from '../interfaces';
-
-export default function shuffleProducts(data: Product[]): Product[] {
+export default function shuffleProducts<T>(data: T[]): T[] {
   const random = data.map(Math.random);
   data.sort((a, b) => random[data.indexOf(a)] - random[data.indexOf(b)]);
   return data;
