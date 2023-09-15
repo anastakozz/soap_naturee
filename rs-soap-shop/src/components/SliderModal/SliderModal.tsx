4c3603932f1d8ad752de87295adc7ee72c828bf5
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-09-15 14:34:22 +0300

    feat: add modal closing on click

--- rs-soap-shop/src/components/SliderModal/SliderModal.tsx
+++ rs-soap-shop/src/components/SliderModal/SliderModal.tsx
@@ -12,12 +12,12 @@ export default function SliderModal(modal: { paths: string[] }) {
   });
   return (
     <>
-      <div className='bg-grayLColor/90 h-full w-full z-[60] absolute top-0 left-0'></div>
-      <div className='absolute z-[70] top-center top-1/2 left-1/2 h-[80%] w-[80%] -translate-x-1/2 -translate-y-1/2'>
+      <div className='bg-grayLColor/90 h-full w-full z-[60] absolute top-0 left-0 back'></div>
+      <div className='absolute z-[70] top-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
         {modal.paths.length === 1 ? (
-          <img src={modal.paths[0]} className='object-contain h-full w-full'></img>
+          <img src={modal.paths[0]} className='object-contain'></img>
         ) : (
-          <CarouselDefault {...{ paths: modal.paths, classes: 'w-full h-full object-contain' }}></CarouselDefault>
+          <CarouselDefault {...{ paths: modal.paths, classes: 'object-contain h-full w-full' }}></CarouselDefault>
         )}
       </div>
       <div className='transition closing-icon absolute top-5 right-5 font-bold text-primaryColor hover:text-accentColor cursor-pointer z-[80]'>
