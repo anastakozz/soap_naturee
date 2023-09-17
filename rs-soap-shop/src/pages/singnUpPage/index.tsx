874a659dc65f7e40228ba8d9a40dfdfc05808f17
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-09-18 02:11:28 +0300

    fix:replace token name

--- rs-soap-shop/src/pages/singnUpPage/index.tsx
+++ rs-soap-shop/src/pages/singnUpPage/index.tsx
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react';
 import { RegistrationForm } from '../../components/forms/RegistrationForm';
 import { useNavigate } from 'react-router-dom';
 import scrollToTop from '../../lib/utils/scrollToTop';
+import { tokenNames } from '../../lib/enums';
 
 function SignUpPage() {
   const navigate = useNavigate();
   useEffect(() => {
     scrollToTop();
-    if (localStorage.getItem('token')) {
+    if (localStorage.getItem(`${tokenNames.userToken}`)) {
       navigate('/');
     }
   }, []);
