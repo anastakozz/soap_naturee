a351297ed3217d14dad50831f0c85c0383e83e1b
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-09-20 22:06:52 +0300

    fix: edit tests

--- rs-soap-shop/src/pages/homePage/randomCardsSection/RandomCardsSection.test.tsx
+++ rs-soap-shop/src/pages/homePage/randomCardsSection/RandomCardsSection.test.tsx
@@ -1,7 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import { CartContext } from '../../../App';
 import RandomCardsSection from './RandomCardsSection';
 
 it('renders random cards section page in DOM', () => {
-  render(<RandomCardsSection />);
+  render(
+    <CartContext.Provider value={[null]}>
+      <RandomCardsSection />
+    </CartContext.Provider>
+  );
   expect(screen.getByRole('random-section')).toBeInTheDocument();
 });
